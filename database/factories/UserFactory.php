b4d2449954244f0383dc259bc1514eac4fa0e810
Jephte Llamzon <62513262+jephtellamzon@users.noreply.github.com>
2023-12-02 11:56:48 +0800

    update user factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,11 +21,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+
+            // add these
+            'user_type' => 'user',
+            'age' => $this->faker->numberBetween(18, 60),
+            'address' => $this->faker->address,
         ];
     }
 
