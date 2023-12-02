f30301e209ce32421dca00bec42224333fc4bf0e
Jephte Llamzon <62513262+jephtellamzon@users.noreply.github.com>
2023-12-02 12:04:57 +0800

    add livewire component and blade files

---
+++ app/Http/Livewire/LiveTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LiveTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.live-table');
+    }
+}
